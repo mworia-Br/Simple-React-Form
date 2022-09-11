6b35d73ce3e6e042cf7ab84fb773de9438cab771
Brian Nganga <66476363+mworia-Br@users.noreply.github.com>
2022-09-11 14:53:24 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,14 @@ import firebase from 'firebase';
 
 // Your web app's Firebase configuration
 var firebaseApp = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: "AIzaSyAKHslUnBOH3wkAmFcyzVQWGEWDEwCHDBk",
+  authDomain: "christine-ecommerce.firebaseapp.com",
+  projectId: "christine-ecommerce",
+  databaseURL: "https:// christine-ecommerce.firebaseio.com",
+  storageBucket: "christine-ecommerce.appspot.com",
+  messagingSenderId: "653111278814",
+  appId: "1:653111278814:web:d4d2101599c651fbb3964f",
+  measurementId: "G-55VN0H6QQB"
 });
 
 var db = firebaseApp.firestore();
